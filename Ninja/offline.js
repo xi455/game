5590952dc63b45f13dcc6f3153bcdeea5eddfc6b
xi455 <79513741+xi455@users.noreply.github.com>
2021-05-07 12:51:54 +0800

    Add files via upload

---
+++ Ninja/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1620363002,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/屁彈-sheet0.png",
+		"images/sprite8-sheet0.png",
+		"images/sprite7-sheet0.png",
+		"images/血量-sheet0.png",
+		"images/血量2-sheet0.png",
+		"images/處之急死-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/tilemap.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
